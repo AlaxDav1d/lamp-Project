fb4b360a01510b2b04bed6c5f83542f9f6762a5e
AlaxDav1d <127566072+AlaxDav1d@users.noreply.github.com>
2023-05-05 22:16:21 -0300

    inicio-

---
+++ lamp/src/App.jsx
@@ -0,0 +1,17 @@
+import Lamp from "./assets/lamp.png";
+import css from "./App.css";
+
+
+
+function App() {
+
+  return (
+    <>      
+      <div className="yellow"></div>
+      <img src={Lamp} width={200 }/> 
+    </>
+
+  )
+}
+
+export default App

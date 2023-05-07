25ea870a0eb289d3eaa1b459de72dbf7fc19fcc2
AlaxDav1d <127566072+AlaxDav1d@users.noreply.github.com>
2023-05-07 16:05:30 -0300

    Add files via upload
    adicionado botões para alterar a imagem

--- lamp/src/App.jsx
+++ lamp/src/App.jsx
@@ -1,16 +1,37 @@
-import Lamp from "./assets/lamp.png";
-import css from "./App.css";
+import { useState } from 'react';
 
+import lamp1 from './assets/lamp1.png';
+import lamp2 from './assets/lamp2.png';
+import style from './App.css';
+import { Switch } from '@mui/material';
 
 
 function App() {
+  const [imgChange,setimgChange] = useState("");
+  
+
+  function lampOn(imgChange){
+    if(imgChange == true){
+      setimgChange(<img src= {lamp2} className='lamp2' />)
+      
+    }
+    else{
+      setimgChange(<img src= {lamp1} className='lamp1'/>)
+    }
+  }
 
   return (
-    <>      
-      <div className="yellow"></div>
-      <img src={Lamp} width={200 }/> 
+    <>
+      <h1>Bonjour le monde</h1>
+      <div className="imagem">
+        {imgChange}
+        <div className="botoes">
+        
+          <button onClick={() => lampOn(false)} className='a1'>desligar a lampada</button>
+          <button onClick={() => lampOn(true)} className='a2'>ligar a lampada</button>
+        </div>
+      </div>
     </>
-
   )
 }
 
